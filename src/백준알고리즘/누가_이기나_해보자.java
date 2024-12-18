036713ba7d69ff370ec0d8b71529474f90919004
namji <namjihun1@gamil.com>
2024-12-18 21:53:05 +0900

    커밋 테스트

---
+++ src/백준알고리즘/누가_이기나_해보자.java
@@ -0,0 +1,16 @@
+package 백준알고리즘;
+
+public class 누가_이기나_해보자 {
+    public static void main(String[] args) {
+        test();
+        test2();
+    }
+    
+    public static void test() {
+        System.out.println("왜 커밋이 안 되는 걸까");
+    }
+    
+    public static void test2() {
+        System.out.println("커밋을 테스트 해보기 위한 프린트");
+    }
+}
